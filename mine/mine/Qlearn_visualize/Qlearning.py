04857bea6a3b04349146b056df21bce80257ad12
masoud <masood.hsn82@gmail.com>
2025-07-11 17:20:17 +0330

    testing and figure out the qlearning algprithm

---
+++ mine/mine/Qlearn_visualize/Qlearning.py
@@ -0,0 +1,69 @@
+﻿import numpy as np
+import random
+from arrayplot import MatrixViewer as mv
+from time import sleep
+#from numpy import savetxt
+
+class QLearning:
+    def __init__(self, env, alpha, gamma, epsilon, epsilon_min, epsilon_dec, episodes):
+        self.env = env
+        self.q_table = np.zeros([env.observation_space.n, env.action_space.n])
+        self.alpha = alpha
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.epsilon_min = epsilon_min
+        self.epsilon_dec = epsilon_dec
+        self.episodes = episodes
+        #self.viewer = mv(self.q_table)
+    
+    def select_action(self, state):
+        randomNumber = random.uniform(0, 1)
+        if randomNumber < self.epsilon:
+            return self.env.action_space.sample() # Explore
+        return np.argmax(self.q_table[state]) # Exploit
+    
+    def train(self, filename):
+        actions_per_episode = []
+        for i in range(1, self.episodes + 1):
+            (state, _) = self.env.reset()
+            reward = 0
+            done = False
+            actions = 0
+            rewards = 0
+
+            while not done:
+                action = self.select_action(state)
+                print(state)
+                
+                for i in range(len(self.q_table[state])):
+                    if i == action:
+                        print(f"[{self.q_table[state,i]}]", end=" ")  
+                    else:
+                        print(self.q_table[state,i], end=" ")  
+
+                print(['down','up','right','left','pick up','drop off'][action])
+                #sleep(2)
+                next_state, reward, done, truncated, terminal = self.env.step(action)
+                
+                print(reward)
+                #self.viewer.update_data(self.q_table)
+
+                old_value = self.q_table[state, action]
+                next_max = np.max(self.q_table[next_state, :])
+                new_value = old_value + self.alpha * (reward + self.gamma * next_max - old_value)
+                self.q_table[state, action] = new_value
+
+                state = next_state
+                actions += 1
+                rewards += reward
+
+            actions_per_episode.append(rewards)
+            if i % 100 == 0:
+                print("Episodes: " + str(i) + "\n")
+
+            if self.epsilon > self.epsilon_min:
+                self.epsilon = self.epsilon * self.epsilon_dec
+
+        #savetxt(filename, self.q_table, delimiter=",")
+        
+        return self.q_table, actions_per_episode
---
+++ mine/mine/Qlearn_visualize/arrayplot.py
@@ -0,0 +1,67 @@
+﻿import numpy as np
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtWidgets, QtCore
+
+class MatrixViewer:
+    def __init__(self, data):
+
+        self.data = data
+        self.rows, self.cols = data.shape
+
+        self.app = QtWidgets.QApplication.instance()
+        if self.app is None:
+            self.app = QtWidgets.QApplication([])
+
+        self.win = pg.GraphicsLayoutWidget()
+        self.view = self.win.addViewBox()
+        self.view.setAspectLocked(True)
+        self.win.show()
+
+        # picture
+        self.img_item = pg.ImageItem(self.data)
+        self.view.addItem(self.img_item)
+
+        lut = pg.colormap.get('viridis').getLookupTable(0.0, 1.0, 256)
+        self.img_item.setLookupTable(lut)
+        self.img_item.setLevels([np.min(self.data), np.max(self.data)])
+
+        
+        self.highlight = pg.RectROI([0, 0], [1, 1], pen=pg.mkPen('red', width=2))
+        self.highlight.setZValue(10)
+        self.highlight.setVisible(False)
+        self.view.addItem(self.highlight)
+
+        self.label = pg.TextItem(anchor=(0, 1), color='w')
+        self.label.setZValue(11)
+        self.view.addItem(self.label)
+
+        # mouse connection
+        self.proxy = pg.SignalProxy(self.view.scene().sigMouseMoved, rateLimit=60, slot=self.mouseMoved)
+
+    def mouseMoved(self, evt):
+        pos = evt[0]  
+        mouse_point = self.img_item.mapFromScene(pos)
+        x, y = int(mouse_point.x()), int(mouse_point.y())
+
+        if 0 <= x < self.cols and 0 <= y < self.rows:
+            val = self.data[y, x]
+            self.label.setText(f"x={x}, y={y}, val={val:.2f}")
+            self.label.setPos(x + 1, y)
+            self.highlight.setPos([x, y])
+            self.highlight.setSize([1, 1])
+            self.highlight.setVisible(True)
+        else:
+            self.highlight.setVisible(False)
+            self.label.setText("")
+
+    def update_data(self, new_data):
+
+        if new_data.shape != (self.rows, self.cols):
+            raise ValueError("اdiffrent shape")
+        self.data = new_data
+        self.img_item.setImage(self.data, autoLevels=False)
+        # update
+        self.img_item.setLevels([np.min(self.data), np.max(self.data)])
+
+    def exec(self):
+        self.app.exec()
