d8b8ae04429e8ca1c71d3f701a887a6212f6b95a
masoud <masood.hsn82@gmail.com>
2025-07-14 17:04:37 +0330

    visualize SARSA algorithm

--- mine/mine/cliffwalker/cliff.py
+++ mine/mine/cliffwalker/cliff.py
@@ -1,32 +1,40 @@
 import gymnasium as gym
 from QLearning import QLearning
+from SARSA import Sarsa
 import numpy as np
 
 env = gym.make("CliffWalking-v0").env  #,render_mode="human").env
 
 # Qlearning
-print("Training using Qlearning")
-qlearning = QLearning(env, 0.9, 0.9, 0.4, 0.05, 0.99, 100)
-q_table, _ = qlearning.train()
+#print("Training using Qlearning")
+#qlearning = QLearning(env, 0.9, 0.9, 0.4, 0.05, 0.99, 100)
+#q_table, _ = qlearning.train()
+
+# SARSA
+print("Training using SARSA")
+sarsa = Sarsa(env, 0.9, 0.9, 0.4, 0.05, 0.99, 100)
+sarsa_table, _ = sarsa.train()
+
 
 
 env = gym.make("CliffWalking-v0", render_mode="human").env
 
-# QLearning
-print("Playing using QLearning")
+print("Playing ...")
 (state, _) = env.reset()
-rewards_q = 0
-actions_q = 0
+rewards = 0
+actions = 0
 done = False
 
 while not done:
-    action = np.argmax(q_table[state])
+    #action = np.argmax(q_table[state])
+    action = np.argmax(sarsa_table[state])
     state, reward, done, truncated, _ = env.step(action)
 
-    rewards_q = rewards_q + reward
-    actions_q += 1
+    rewards = rewards + reward
+    actions += 1
     
 
 # Result
-print(f"Actions by Qlearning: {actions_q}")
-print(f"Rewards for Qlearning: {rewards_q}")
+print(f"Actions: {actions}")
+print(f"Rewards: {rewards}")
+
